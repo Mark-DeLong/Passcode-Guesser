cda0342f00c5fabc36c2840498937c27bdae439c
Mark <mark@Marks-MacBook-Air.local>
2020-11-24 11:47:48 -0500

    cleaned up formatting

--- main.c
+++ main.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long password, guess;
+long long password, guess, i;
 
 int main () {
 
     printf("Enter a numerical passcode \n");
     scanf("%lli", &password);
-    
-    long long i;
                     
     for (i = 0; i < 55000000000000; i++) {
         guess = i;
         
-        
-
         if (guess == password) {
             printf("Your passcode is %lli", guess);
             exit(0);
